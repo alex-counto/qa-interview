7fc4e2ecfd9cf3c12f21d73c4b4dc67646786c1e
start.vaadin.com <no-reply@vaadin.com>
2023-12-27 23:53:43 +0000

    Generated project

---
+++ src/main/java/com/xion/interview/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.xion.interview.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/xion/interview/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.xion.interview.services;
+
+import org.springframework.lang.NonNullApi;
